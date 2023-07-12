5eae619174893ec5a440df2e527402155e14998b
Dmytro Hushchyk <d.hushchyk@gmail.com>
2023-07-12 22:46:02 +0300

    changed config

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ if "AWS_STORAGE_BUCKET_NAME" in os.environ:
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    AWS_STORAGE_BUCKET_NAME = "itemsdg-media"
+    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
     AWS_S3_REGION_NAME = os.environ["AWS_S3_REGION_NAME"]
 
     AWS_S3_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
