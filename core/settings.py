b366599a6a789a59c6546a34753b180def246fcd
Dmytro Hushchyk <d.hushchyk@gmail.com>
2023-07-12 23:04:18 +0300

    added corsheaders

--- core/settings.py
+++ core/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "storages"
+    "storages",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -154,3 +156,6 @@ if "AWS_STORAGE_BUCKET_NAME" in os.environ:
     AWS_S3_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
     AWS_S3_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
     AWS_DEFAULT_ACL = "public-read-write"
+
+
+CORS_ALLOWED_ORIGINS = ["*"]
